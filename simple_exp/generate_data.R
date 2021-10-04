9a6405acf2752ae3291c3c1876a3f5d161a6d0bf
ginberg <info@gerinberg.com>
2021-10-04 13:54:48 +0000

    add simple_exp data

---
+++ simple_exp/generate_data.R
@@ -0,0 +1,18 @@
+library(dplyr)
+
+# parameters settings
+num_vars     <- 10
+num_obs      <- 10
+total_vals   <- num_vars * num_obs
+meas_min_val <- 0
+meas_max_val <- 1
+
+# generate data
+var_vals    <- sprintf(paste0("variable%0", nchar(as.character(num_vars)), "d"), 1:num_vars)
+obs_vals    <- sort(rep(sprintf(paste0("observation%0", nchar(as.character(num_obs)), "d"), 1:num_obs), num_vars))
+
+data <- data.frame(Observation = obs_vals, 
+                   Variable = var_vals, 
+                   Measurement = runif(total_vals, meas_min_val, meas_max_val))
+
+write.table(data, "simple_exp/simple_exp.tsv", sep = "\t", row.names = FALSE)
