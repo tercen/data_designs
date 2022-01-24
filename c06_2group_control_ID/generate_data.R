4668edd5bd5f1badc17c625ff757b1d74d980ee5
ginberg <info@gerinberg.com>
2022-01-24 09:55:22 +0100

    update UKA dataset

--- c06_2group_control_ID/generate_data.R
+++ c06_2group_control_ID/generate_data.R
@@ -3,19 +3,18 @@ library(dplyr)
 library(tidyr)
 
 # set parameters settings
-supergroup <- "Supergroup"
+
 group      <- "Group"
 observation<- "Observation"
-variable   <- "Variable" 
 measurement<- "Measurement"
-
+ID         <- "ID"
 
 num_controls   <- 4
 num_treatments <- 4
 num_obs        <- num_controls + num_treatments
-num_vars       <- 100
-num_super_groups <- 2
-
+num_vars       <- 10
+ID_values      <- c("EFS_246_258", "41_654_666", "ANXA2_17_29", "CBL_693_705", "CD3Z_116_128", 
+                    "CDK2_8_20", "CTNB1_79_91", "DCX_67_79", "DDR1_506_518", "DYR1A_312_324")
 
 fparams <- data.frame(m1 = num_controls, m2 = num_treatments, shape2 = 4, lb = 4, ub = 14, pde=0.02, sym=0.5);
 dparams <- data.frame(lambda1 = 0.13, lambda2 = 2, muminde = 1, sdde = 0.5);
@@ -23,39 +22,20 @@ sdn     <- 0.4
 rseed   <- 42
 
 
-# control <- rep("control", num_controls)
-# treatment <- rep("treatment", num_treatments)
-# Group <- c(control,treatment)
-# 
-# Variable <- sprintf(paste0("var%0", nchar(as.character(num_vars)), "d"), 1:num_vars)
-# Observation <- sprintf(paste0("obs%0", nchar(as.character(num_obs)), "d"), 1:num_obs)
-# Supergroup <- sprintf(paste0("supergroup%0", nchar(as.character(num_super_groups)), "d"), 1:num_super_groups)
-# 
-# annot_df <- data.frame(Group, Supergroup)
-# annot_df <- annot_df %>% arrange(Supergroup)
-
 # generate fully synthetic data
-get_group_data <- function(supergroup) {
-  data_gen <- madsim(mdata = NULL, n = num_vars, ratio = 0, fparams, dparams, sdn, rseed)
-  data_gen$xdata %>% 
-    as_tibble() %>% 
-    mutate(Variable = sprintf(paste0("var%0", nchar(as.character(num_vars)), "d"), 1:nrow(.)))%>%
-    pivot_longer(c(everything(), -Variable), names_to = observation, values_to = measurement) %>%
-    mutate(Group = case_when(
-      startsWith(Observation, "cont") ~ "control",
-      startsWith(Observation, "test") ~ "treatment")) %>%
-    mutate(Supergroup = paste0("supergroup", supergroup)) %>%
-    select(Variable, Group, Supergroup, Measurement)
-}
+data_gen <- madsim(mdata = NULL, n = num_vars, ratio = 0, fparams, dparams, sdn, rseed)
+data     <- data_gen$xdata %>% 
+  as_tibble() %>%
+  top_n(10) %>%
+  mutate(ID = ID_values) %>%
+  pivot_longer(c(everything(), -ID), names_to = observation, values_to = measurement) %>%
+  mutate(Group = case_when(
+    startsWith(Observation, "cont") ~ "control",
+    startsWith(Observation, "test") ~ "treatment")) %>%
+  mutate(Observation = rep(sprintf(paste0("obs%0", nchar(as.character(num_obs)), "d"), 1:(num_obs)), num_vars)) %>%
+  select(Observation, ID, Group, Measurement)
 
-Observation <- rep(sprintf(paste0("obs%0", nchar(as.character(num_obs)), "d"), 1:(num_obs*num_super_groups)), num_vars)
-
-data <- do.call(rbind, lapply(seq(num_super_groups), FUN = function(x) {get_group_data(x)})) %>%
-  arrange(Variable) %>%
-  mutate(Observation = Observation) %>%
-  select(Observation, Variable, Group, Supergroup, Measurement)
-
-write.table(data, "c06_super_2group_control/c06_super_2group_control.tsv", sep = "\t", row.names = FALSE)
+write.table(data, "c06_2group_control_ID/c06_2group_control_ID.tsv", sep = "\t", row.names = FALSE)
 
 
 
